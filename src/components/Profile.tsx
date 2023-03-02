c90d564470ce9c219aadbf169019b6edb2e5a0d1
Alex62510 <eagle625@tut.by>
2023-03-02 16:26:35 +0300

    commit12Done

---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+    return(<div className={"content"}>
+        <div>
+            <img
+                src={"https://wonder-day.com/wp-content/uploads/2020/04/wonder-day-images-rainbow-37-1024x576.jpg"}/>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            my posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>)
+
+}
+export default Profile;
