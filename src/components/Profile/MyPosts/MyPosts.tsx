270f7526fec288eb14f037638f51c481882f0e38
Alex62510 <eagle625@tut.by>
2023-03-30 13:47:09 +0300

    commit19Done

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts=()=>{
+const MyPosts=(props:any)=>{
     return(
     <div>
         my posts
@@ -10,11 +10,8 @@ const MyPosts=()=>{
             New post
         </div>
         <div className={s.posts}>
-           <Post/>
-           <Post/>
-           <Post/>
-           <Post/>
-           <Post/>
+           <Post message={"Hi how are you?"}/>
+           <Post message={"It's my first post"}/>
         </div>
     </div>
     )
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import s from './Post.module.css'
 
-const Post = () => {
+
+const Post = (props:any) => {
     return (
         <div className={s.item}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShwIXO7TuAocUZF1Jq5mJuRs78wGsf3Qe5-w&usqp=CAU"/>
-            post 1
+            {props.message}
             <div>
                 <span>likes</span>
             </div>
