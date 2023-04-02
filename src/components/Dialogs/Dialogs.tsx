2446c766896abe3104b5a1691a83601d7d7bee09
Alex62510 <eagle625@tut.by>
2023-04-02 23:08:58 +0300

    commit22Done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Dimich</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Victor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6'>Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}> Hi</div>
+                <div className={s.dialog}>How are you</div>
+                <div className={s.dialog}> Yo</div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
             <NavLink to='/Profile' activeClassName={s.activeLink}>Profile</NavLink>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <NavLink to='/Dialogs' activeClassName={s.activeLink}>Messages</NavLink>
+            <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to='/News' activeClassName={s.activeLink}>News</NavLink>
@@ -17,7 +17,7 @@ function Navbar() {
             <NavLink to='Music' activeClassName={s.activeLink}>Music</NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='Settings' activeClassName={s.activeLink}>Stttings</NavLink>
+            <NavLink to='Settings' activeClassName={s.activeLink}>Settings</NavLink>
         </div>
     </nav>)
 }
