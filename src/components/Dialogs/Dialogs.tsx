94e2e8c0dbfec436494e33d46aeb9beb6790279b
Alex62510 <eagle625@tut.by>
2023-04-03 23:27:09 +0300

    commit24Done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,33 +2,35 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+
+const DialogItem = (props: any) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message = (props: any) => {
+    return (
+        <div className={s.dialog}> {props.message}</div>
+    )
+}
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to='/dialogs/1'>Dimich</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/2'>Andrey</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/3'>Sveta</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/4'>Sasha</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/5'>Victor</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/6'>Valera</NavLink>
-                </div>
+                <DialogItem name='Dimich' id="1"/>
+                <DialogItem name='Andrey' id="2"/>
+                <DialogItem name='Sveta' id="3"/>
+                <DialogItem name='Sasha' id="4"/>
+                <DialogItem name='Victor' id="5"/>
+                <DialogItem name='Valera' id="6"/>
             </div>
             <div className={s.messages}>
-                <div className={s.dialog}> Hi</div>
-                <div className={s.dialog}>How are you</div>
-                <div className={s.dialog}> Yo</div>
+                <Message message={'Hi'}/>
+                <Message message={'How are you'}/>
+                <Message message={'Yo'}/>
             </div>
         </div>
     );
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import s from './Profile.module.css'
 import Post from "./MyPosts/Post/Post";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Profile() {
     return (
         <div className={s.content}>
-            <div>
-                <img
-                    src={"https://wonder-day.com/wp-content/uploads/2020/04/wonder-day-images-rainbow-37-1024x576.jpg"}/>
-            </div>
-            <div>
-                ava+description
-            </div>
+            <ProfileInfo/>
             <MyPosts />
         </div>
     )
