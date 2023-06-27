fb82e741ddbdfb30473855bbcc31d8c17c8c1ece
Alex62510 <eagle625@tut.by>
2023-06-27 23:01:28 +0300

    commit75 Done

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,13 +5,15 @@ import sideBarReducer from "./sideBar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import  thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from "redux-form"
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     messagePage: messageReducer,
     sideBar: sideBarReducer,
     usersPage:usersReducer,
-    auth:authReducer
+    auth:authReducer,
+    form:formReducer
 })
 export type AppStateType=ReturnType<typeof rootReducer>
 export let store = createStore(rootReducer,applyMiddleware(thunkMiddleware))
