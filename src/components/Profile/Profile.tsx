fed029a80ed4ec612ec564d593d05de1493f1e8d
Alex62510 <eagle625@tut.by>
2023-08-07 13:15:24 +0300

    commit92_ react renderer

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import s from './Profile.module.css'
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import MyPostsContainer from "./MyPosts/Post/MyPostsContainer";
-import {ProfileType} from "../../redux/profile-reducer";
 import SuperMyPostContainer from "./MyPosts/Post/MyPostsContainer";
+import {ProfileType} from "../../redux/profile-reducer";
 
 
 export type ProfilepropsType = {
