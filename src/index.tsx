452393052a549634529c270419e3fecfd564e1b8
Alex62510 <eagle625@tut.by>
2023-09-20 21:32:40 +0300

    commit101_add boddy plagin

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,16 @@ import './index.css';
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.render(
-        <Provider store={store}>
-            <App />
-        </Provider>,
-        document.getElementById('root')
-    )
+    <Provider store={store}>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </Provider>,
+    document.getElementById('root')
+)
