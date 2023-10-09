c847268836488ae0f36849305eddecdff16c7bbc
Alex62510 <eagle625@tut.by>
2023-10-09 15:30:48 +0300

    commit109_ test user-reduser

---
+++ src/redux/users-reduser.test.ts
@@ -0,0 +1,14 @@
+import {InitialUsersStateType} from "./users-reducer";
+
+test("", () => {
+        const state:InitialUsersStateType = {
+            users: [],
+            pageSize: 10,
+            totalUsersCount: 0,
+            currentPage: 1,
+            isFetching: false,
+            followInProgress: []
+        }
+        // expect()
+    }
+)
