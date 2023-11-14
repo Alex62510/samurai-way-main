c6d4dd67e7d27dfe36996ecad8e82a35762de426
Alex62510 <eagle625@tut.by>
2023-11-14 10:57:42 +0300

    118 add WebSocet

--- src/pages/chat/chatPage.tsx
+++ src/pages/chat/chatPage.tsx
@@ -1,29 +1,51 @@
 import React, {FC} from 'react';
+import style from "./chatPage.module.css"
 
-const ChatPage:FC = () => {
+const ws=new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+
+const ChatPage: FC = () => {
     return (
         <div>
             <Chat/>
         </div>
     );
 };
-const Chat:FC=()=>{
-    return(
+const Chat: FC = () => {
+
+
+    return (
         <div>
             <Messages/>
             <AddMessageForm/>
         </div>
     )
 }
-const Messages:FC=()=>{
-    return(
-        <div>
-           Messages
+const Messages: FC = () => {
+    const messages: any[] = [1,2,3,4,5,6,7]
+    return (
+        <div className={style.messages}>
+            {messages.map((m) => <Message/>)}
         </div>
     )
 }
-const AddMessageForm:FC=()=>{
-    return(
+const Message: FC = () => {
+    const message = {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYxjy_n63skpStAoGo8O83ma5kzGCr1aI_QMKBmy6REnwAdBYK4EdfeetZYOPGuvn0Z-s&usqp=CAU",
+        author:"Alex",
+        text: "Hi"
+    }
+    return (
+        <div>
+            <img src={message.url} className={style.avatar}/>
+            <b>{message.author}</b>
+            <br/>
+            {message.text}
+            <hr/>
+        </div>
+    )
+}
+const AddMessageForm: FC = () => {
+    return (
         <div>
             <div>
                 <textarea></textarea>
@@ -31,8 +53,8 @@ const AddMessageForm:FC=()=>{
             <div>
                 <button>Send</button>
             </div>
-            </div>
-            
+        </div>
+
 
     )
 }
